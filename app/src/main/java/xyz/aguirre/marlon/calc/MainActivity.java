d63acf817494a5996e1109e58862c87e2cb3facb
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2016-11-30 00:30:05 -0600

    basic functions complete

--- app/src/main/java/xyz/aguirre/marlon/calc/MainActivity.java
+++ app/src/main/java/xyz/aguirre/marlon/calc/MainActivity.java
@@ -1,40 +1,142 @@
 package xyz.aguirre.marlon.calc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.inputBill)
+    EditText inputBill;
+    @BindView(R.id.btnSubmit)
+    Button btnSubmit;
+    @BindView(R.id.inputPercentage)
+    EditText inputPercentage;
+    @BindView(R.id.btnIncrease)
+    Button btnIncrease;
+    @BindView(R.id.btnDecrease)
+    Button btnDecrease;
+    @BindView(R.id.btnClear)
+    Button btnClear;
+    @BindView(R.id.txtTip)
+    TextView txtTip;
+    /*
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    <android.support.v7.widget.RecyclerView
+        android:id="@+id/recyclerView"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:layout_below="@+id/txtTip" />
+
+    */
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE =10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.action_about){
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void about(){
-        TipCalcApp app = (TipCalcApp)getApplication();
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmit(){
+        hideKeyBorad();
+        String strInputTotal = inputBill.getText().toString().trim();
+        if(!strInputTotal.isEmpty()){
+            double total = Double.parseDouble(strInputTotal);
+            int tipPercentage = getTipPercentege();
+            double tip = total *(tipPercentage/100d);
+
+            String strTip = String.format(getString(R.string.global_message),tip);
+            txtTip.setVisibility(View.VISIBLE);
+            txtTip.setText(strTip);
+        }
+    }
+
+    @OnClick(R.id.btnIncrease)
+    public void handleClickIncrease(){
+        hideKeyBorad();
+        handleTipChange(TIP_STEP_CHANGE);
+    }
+
+    @OnClick(R.id.btnDecrease)
+    public void handleClickDecrease(){
+        hideKeyBorad();
+        handleTipChange(-TIP_STEP_CHANGE);
+    }
+
+    private void handleTipChange(int change) {
+        int tipPercentage = getTipPercentege();
+        tipPercentage += change;
+        if (tipPercentage >0){
+            inputPercentage.setText(String.valueOf(tipPercentage));
+        }
+    }
+
+    private int getTipPercentege() {
+        int tipPercentage = DEFAULT_TIP_PERCENTAGE;
+        String strInputTipPercentage = inputPercentage.getText().toString().trim();
+        if(!strInputTipPercentage.isEmpty()){
+            tipPercentage = Integer.parseInt(strInputTipPercentage);
+        }else{
+            inputPercentage.setText(String.valueOf(tipPercentage));
+        }
+        return tipPercentage;
+    }
+
+    private void hideKeyBorad() {
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }catch (NullPointerException npe){
+            Log.e(getLocalClassName(),Log.getStackTraceString(npe));
+        }
+    }
+
+    private void about() {
+        TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getUrl();
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(strUrl));
         startActivity(intent);
     }
+
+
 }
 

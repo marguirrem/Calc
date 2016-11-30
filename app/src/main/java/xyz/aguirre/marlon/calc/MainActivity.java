546ad4f4e6ec8650879181ad9909b09452463cc1
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2016-11-29 22:23:29 -0600

    menu and action about

--- app/src/main/java/xyz/aguirre/marlon/calc/MainActivity.java
+++ app/src/main/java/xyz/aguirre/marlon/calc/MainActivity.java
@@ -1,7 +1,11 @@
 package xyz.aguirre.marlon.calc;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.action_about){
+            about();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void about(){
+        TipCalcApp app = (TipCalcApp)getApplication();
+        String strUrl = app.getUrl();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(strUrl));
+        startActivity(intent);
+    }
 }
+
---
+++ app/src/main/java/xyz/aguirre/marlon/calc/TipCalcApp.java
@@ -0,0 +1,16 @@
+package xyz.aguirre.marlon.calc;
+
+import android.app.Application;
+
+/**
+ * Created by marlon on 29/11/16.
+ */
+
+public class TipCalcApp extends Application {
+
+    private static final String url = "https://gt.linkedin.com/in/marguirrem";
+
+    public String getUrl() {
+        return url;
+    }
+}
